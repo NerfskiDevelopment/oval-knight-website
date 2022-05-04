7cc74dfc7179ff928453070e3f6ea8a930361b54
NerfskiDevelopment <64707438+NerfskiDevelopment@users.noreply.github.com>
2022-05-03 20:35:10 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -22,6 +22,34 @@ function LoadData(){
       }
       
       document.getElementById("top10").innerHTML = inner;
+  }
+}
+
+}
+function LoadEKills(){
+  const http = new XMLHttpRequest();
+  const url = 'https://236f935f-8f97-4113-a394-ae28e91900a2.id.repl.co/TopE';
+  
+  http.open("GET", url);
+  http.send();
+
+  http.onreadystatechange=function(){
+    if(this.readyState==4 && this.status == 200){
+      var rawJson = JSON.parse(http.responseText);
+
+      var inner = "";
+      
+      for(var i = 0; i < rawJson['L'].length; i++){
+        var object = rawJson['L'][i];
+        
+        var name = object['name'];
+        var kills = object['e'];
+        var placement = object['placement'];
+
+        inner += "<div id='listing'><h1 id='l'>&nbsp;#" + (i+1) + "</12>&nbsp;&nbsp;&nbsp;<h2>" + kills + "</h2><h3>" + name + "</h3></div>"
+      }
+      
+      document.getElementById("tope").innerHTML = inner;
     }
   }
 }
